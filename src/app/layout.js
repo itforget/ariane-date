d5e30af174162bb318bb4f1721b9eb2bf138cf91
itforget <itforget11@gmail.com>
2023-12-24 13:49:18 -0300

    change animation

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Roboto_Condensed } from 'next/font/google'
 import './globals.css'
 import Flores from "./components/flowers"
 
+
 const inter = Roboto_Condensed({ 
   weight: ['400', '500', '600', '700', '800', '900'],
   subsets: ['latin'] })
@@ -14,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-pink-300`}>{children}
+      <body className={`${inter.className} bg-pink-200`}>{children}
       <Flores />
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,66 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
- const [mostrarTexto, setMostrarTexto] = useState(false);
+  const [index, setIndex] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
- const alternarTexto = () => {
-    setMostrarTexto(!mostrarTexto);
- };
+  const alternarTexto = () => {
+    setIndex((index + 1) % textos.length);
+    
+    // Se chegarmos ao último elemento do array, desativamos o autoPlay para parar a transição
+    if (index === textos.length - 2) {
+      setAutoPlay(false);
+    }
+  };
 
- return (
+  const textos = [
+    'ESTE ANO',
+    'ALGO INCRIVEL',
+    'ACONTECEU', 'NA MINHA',
+    'VIDA...', 'TER TE CONHECIDO',
+    'FOI A MELHOR COISA',
+    'QUE ACONTECEU ESSE ANO',
+    'MAS...',
+    'AINDA ESTA',
+    'FALTANDO UMA COISA',
+    'PRA FICAR AINDA MELHOR',
+    'QUERO TE PEDIR UMA COISA',
+    'PREPARADA???',
+    '5...', '4...', '3...', '2...', '1...',
+    'QUER NAMORAR COMIGO?'
+  ];
+
+  useEffect(() => {
+    let intervalId;
+
+    if (autoPlay) {
+      setIsVisible(true);
+
+      intervalId = setInterval(() => {
+        alternarTexto();
+      }, 2000); // 1000 milissegundos (1 segundo) - ajuste conforme necessário
+    }
+
+    return () => clearInterval(intervalId);
+  }, [autoPlay, index]);
+
+  const handleButtonClick = () => {
+    setAutoPlay(true);
+    setIsVisible(false); // Esconde o texto ao clicar no botão
+  };
+
+  return (
     <div className="flex place-content-center p-24 flex-col">
-      <h1 className={`${mostrarTexto ? '' : 'hidden'} flex font-extrabold text-6xl place-content-center`}>
-        QUER NAMORAR COMIGO?
+      <h1
+        className={`flex font-extrabold text-6xl place-content-center animate-jump-in animate-infinite animate-duration-[2000ms] ${isVisible ? 'visible' : 'hidden'}`}
+      >
+        {textos[index]}
       </h1>
-      <button onClick={alternarTexto} className={`${mostrarTexto ? 'hidden' : ''} flex rounded-2xl text-2xl font-extrabold place-content-center border-2 hover:opacity-55`}>
+      <button onClick={handleButtonClick} className={`flex rounded-2xl text-2xl font-extrabold place-content-center border-2 hover:opacity-55 ${isVisible ? 'hidden' : 'visible'}`}>
         APERTE
       </button>
     </div>
- );
+  );
 }
